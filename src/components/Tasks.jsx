ebd95e8bdf01d0333f55adef0c0ca716b8b79a3f
Amanda <amandaambrosio295@gmail.com>
2025-02-24 11:17:56 -0300

    adicionei a parte de inserir tarefas, biblioteca uuid que cria ID aleatório, adcionei a função de remover e adcionar tarefas, instalei biblioteca de icones do react levide e também quando a task é clicada é marcada como concluída

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,22 @@
-function Tasks(props){
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks({tasks, onTaskClick, onDeleteTaskClick}){
 return (
 <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-    {props.tasks.map((task) => (
-        <li key={task.id} className="flex">
-        <button className="bg-slate-400 w-full text-white p-2 rounded-md ">{task.title}</button>
-        <button>Ver detalhes</button>
+    {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+        <button 
+        onClick={() => onTaskClick(task.id)} className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}
+            >{task.title}
+        </button>
+        <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+        </button>
+        <button
+        onClick={() => onDeleteTaskClick(task.id)}
+         className="bg-slate-400 p-2 rounded-md text-white">
+            <TrashIcon />
+        </button>
         </li>
         ))}
     </ul>
