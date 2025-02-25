61fbdaf00544c65625e1fc212d89b21137ad3533
Amanda <amandaambrosio295@gmail.com>
2025-02-25 16:46:03 -0300

    estilizei e adcionei a funcionalidade da aba de descrição da task

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,7 +1,27 @@
-function TaskPage () {
-return <div className="h-screen w-screen bg-slate-500 p-6">
+import { useSearchParams } from "react-router-dom";
 
-</div>
+function TaskPage() {
+  const [searchParams] = useSearchParams();
+
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <div className="w-[500px] space-y-4">
+        
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Detalhes da Tarefa
+        </h1>
+
+        <div className="bg-slate-400 p-4 rounded-md">
+        <h1 className="text-xl text-white font-bold">{title}</h1>
+        <p className="text-white">{description}</p>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
-export default TaskPage
+export default TaskPage;
