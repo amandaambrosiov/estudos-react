22f4ae201969a3cfc94d780b4d6ac9a9762637eb
Amanda <amandaambrosio295@gmail.com>
2025-02-28 15:34:40 -0300

    estilizando página de descrição da task

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,6 +1,9 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
+const navigate = useNavigate()
+
   const [searchParams] = useSearchParams();
 
   const title = searchParams.get("title");
@@ -10,13 +13,19 @@ function TaskPage() {
     <div className="h-screen w-screen bg-slate-500 p-6">
       <div className="w-[500px] space-y-4">
         
+        <div className="flex justify-center relative mb-6">
+          <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
+            <ChevronLeftIcon />
+          </button>
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Detalhes da Tarefa
         </h1>
 
-        <div className="bg-slate-400 p-4 rounded-md">
-        <h1 className="text-xl text-white font-bold">{title}</h1>
-        <p className="text-white">{description}</p>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+        <h1 className="text-xl font-bold text-slate-600">{title}</h1>
+        <p className="text-slate-600">{description}</p>
         </div>
 
       </div>
